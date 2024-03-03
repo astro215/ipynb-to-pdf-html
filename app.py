55e71aab12b724e348a6678b081ad40ef68d1bb4
Jainil Patel <jainil.patel.btech2021@sitpune.edu.in>
2024-03-04 01:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import streamlit as st
 from nbconvert import HTMLExporter, PDFExporter
 from nbformat import read
+import os
 
 st.title("IPython Notebook Converter")
 
 # Function to convert IPython Notebook to HTML
-def convert_to_html(notebook):
+def convert_to_html(notebook, filename):
     html_exporter = HTMLExporter()
     (body, resources) = html_exporter.from_notebook_node(notebook)
-    return body
+    html_filename = f"{filename}.html"
+    with open(html_filename, "w") as html_file:
+        html_file.write(body)
+    return html_filename
 
 # Function to convert IPython Notebook to PDF
-def convert_to_pdf(notebook):
+def convert_to_pdf(notebook, filename):
     pdf_exporter = PDFExporter()
     (body, resources) = pdf_exporter.from_notebook_node(notebook)
-    return body
+    pdf_filename = f"{filename}.pdf"
+    with open(pdf_filename, "wb") as pdf_file:
+        pdf_file.write(body)
+    return pdf_filename
 
 # Main function
 def main():
@@ -22,20 +29,21 @@ def main():
 
     if uploaded_file is not None:
         notebook = read(uploaded_file, as_version=4)
+        filename = os.path.splitext(uploaded_file.name)[0]
 
         if st.button("Convert to HTML"):
-            html_content = convert_to_html(notebook)
-            # st.markdown(html_content, unsafe_allow_html=True)
-            st.markdown(get_download_link(html_content, 'html', 'Download HTML'), unsafe_allow_html=True)
+            html_filename = convert_to_html(notebook, filename)
+            st.markdown(get_download_link(html_filename, 'html', 'Download HTML'), unsafe_allow_html=True)
 
         if st.button("Convert to PDF"):
-            pdf_content = convert_to_pdf(notebook)
-            # st.markdown(pdf_content, unsafe_allow_html=True)
-            st.markdown(get_download_link(pdf_content, 'pdf', 'Download PDF'), unsafe_allow_html=True)
+            pdf_filename = convert_to_pdf(notebook, filename)
+            st.markdown(get_download_link(pdf_filename, 'pdf', 'Download PDF'), unsafe_allow_html=True)
 
 # Function to create download link
-def get_download_link(content, file_format, text):
-    href = f'<a href="data:file/{file_format};base64,{content}" download="output.{file_format}">{text}</a>'
+def get_download_link(file_path, file_format, text):
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+    href = f'<a href="data:file/{file_format};base64,{file_content}" download="{os.path.basename(file_path)}">{text}</a>'
     return href
 
 if __name__ == "__main__":
